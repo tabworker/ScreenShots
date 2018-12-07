b7d44bf067b2f7227336bdf0d9798d5c48df92ee
Alexander Shilov <45393617+tabworker@users.noreply.github.com>
2018-12-07 12:02:47 +0300

    HTScreen.vba
    Загрузка файла HTScreen

---
+++ HTScreen.vb
@@ -0,0 +1,29 @@
+﻿REM HTScreen
+
+Sub HTScreen()
+'
+' HTScreen Макрос
+' HTScreen (Hyper Text Screen) - программный модуль (макрос), для автоматического создания гипертекстовой ссылки в электронной таблице Microsoft Office Excel.
+'
+
+Dim adress As String
+Dim element As String
+Dim stlb As String
+Dim strok As Integer
+Dim link As String
+
+stlb = InputBox("Введите букву столбца с номерами скриншота")
+k = InputBox("Введите количество строк в таблице")
+
+For strok = 2 To k
+    link = stlb + CStr(strok)
+    Range(link).Select
+    element = Range(link).Value
+        If element <> "" Then
+         adress = "[path]\" + element + ".jpg"
+                ActiveSheet.Hyperlinks.Add Anchor:=Selection, Address:= _
+            adress
+        End If
+Next strok
+
+End Sub
